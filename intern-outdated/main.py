da203f0efa4a68447033698d5802a642ee842df3
electronyoon <electronyoon@gmail.com>
2020-11-27 09:57:36 +0900

    .

--- intern-outdated/main.py
+++ intern-outdated/main.py
@@ -1,170 +1,104 @@
-from urllib.request import urlopen
-from urllib.request import Request
-from urllib import parse
-from urllib.error import HTTPError
+from openpyxl import load_workbook
+from datetime import datetime as dt
+from tqdm import tqdm
 
 import pandas as pd
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
-
 import freenote as fn
 import time
 
-driver = webdriver.Chrome(executable_path=r'C:\chromedriver.exe')
+def removeBlanks(s):
+    while True:
+        if " " in s:
+            s = s.replace(" ", "")
+        else:
+            break
+    return s
 
-# address = "서울특별시 종로구 당주동 160"
-
-# d = fn.StringHandler.addressToDict(address)
-# print(d)
-# r = fn.Action.isplSearch(driver, d)
-
-# # address_pieces = fn.StringHandler.addressToDict(address)
-# # fn.Action.isplSearch(driver, )
-# # address_ispl_info = fn.Action.isplPopupCheck(driver, d)
-# # d = fn.Action.isplVerify(address, d)
-# print(r)
-
-# def naverSearch(dict):
-#     driver.get('http://map.naver.com/')
-#     driver.implicitly_wait(3)
-#     driver.find_element_by_xpath('''//div[@class="input_box"]/input''').clear()
-#     time.sleep(0.5)
-#     driver.find_element_by_xpath('''//div[@class="input_box"]/input''').send_keys(address)
-#     time.sleep(0.5)
-#     driver.find_element_by_xpath('''//div[@class="input_box"]/input''').send_keys(Keys.RETURN)
-#     time.sleep(0.5)
-#     try:
-#         # 검색 결과의 XPATH가 존재하는지 확인
-#         driver.find_element_by_xpath('''//div[@class="summary_area"]''')
-#     except:
-#         print("Naver search: XAPTH class 'summary_area' not found.")
-#         dict['naver_result'].append(False)
-#     else:
-#         # 검색 결과의 XPATH에 text가 로딩될 때까지 while 대기반복
-#         while not driver.find_element_by_xpath('''//div[@class="summary_area"]''').text:
-#             time.sleep(0.1)
-#         # 검색 결과 text가 검색하고자 하는 주소와 동일한지 separateBungee 메서드를 이용해 검증, 실패시 FALSE 기록
-#         temp_dict = seperateBungee(address)
-#         temp_keylist = list(temp_dict.keys())
-#         for i in range(4):
-#             if temp_list(temp_keylist[i]) == dict(temp_keylist[i]):
-#                 pass
-#             else:
-#                 print("The address does not correspond to original one.")
-#                 dict['naver_result'].append(False)
-#                 return dict
-#         # 검증 완료 시 TRUE 기록
-#         print("Naver search: address found. " + driver.find_element_by_xpath('''//div[@class="summary_area"]''').text)
-#         dict['naver_result'].append(True)
-#     return dict
-
-
-
-
-
-
-# # try:
-# #     element = WebDriverWait(driver, 10).until(
-# #         EC.presence_of_element_located((By.XPATH, '''//div[@class="summary_area"]'''))
-# #     )
-# # finally:
+def compareList(l):
+    # remove list with unexisting ratio
+    for i in l:
+        if not isinstance(i[1], str):
+            l.remove(i)
     
-# #     driver.quit()
+    # compare dates
+    datetime_list = []
+    for i in l:
+        d = dt.strptime(i[2], "%Y-%m-%d")
+        datetime_list.append(d)
+    try:
+        youngest = max(datetime_list).strftime('%Y-%m-%d')
+    except:
+        return 0
 
+    # find news
+    result_list = []
+    for i in l:
+        if i[2] == youngest:
+            result_list.append(i)
 
+    return result_list
 
+# addr_column = "A"
+# addr_to_num = sum([v*26**(len(addr_column)-i-1) for i, v in enumerate([ord(s)-64 for s in addr_column])])-1
+print("reading approved excel file as pandas...")
+data = pd.read_excel(r'C:\Users\user\Desktop\사용(임시)승인허가현황조회 전체_강서구_정렬_1120.xls')
+df = data.loc[:, ['건축구분', '대지위치', '용적률(%)', '사용승인일']]
+l = df.values.tolist()
+for i in l:
+    i[1] = removeBlanks(i[1])
 
+apprd = {}
+for i in l:
+    address = i[1]
+    if address in apprd:
+        i.remove(address)
+        apprd[address].append(i)
+    else:
+        apprd[address] = []
+        i.remove(address)
+        apprd[address].append(i)
 
+print("reading target excel file as pandas...")
+data2 = pd.read_excel(r'C:\Users\user\Desktop\뉴딜데이터정비_건축물대장_강서구_표시현황정비(대지면적외항목)_201126_검수1_김영철김소현.xlsx',
+                    sheet_name='용적률')
+df2 = data2.iloc[:, 2:4]
+l2 = df2.values.tolist()
+del l2[0]
+tl = []
+for i in l2:
+    tl.append(removeBlanks(str(i[0])+str(i[1])))
 
-
-
-   
-
-addr_column = "A"
-addr_to_num = sum([v*26**(len(addr_column)-i-1) for i, v in enumerate([ord(s)-64 for s in addr_column])])-1
-data = pd.read_excel(r'C:\Users\user\Desktop\temp.xlsx', header=None)
-df = data.iloc[:,addr_to_num]
-givenaddr_list = df.values.tolist()
-dataframe_result = fn.LandInfoStructure.dataframe_result
-
-
-# driver.get('https://www.juso.go.kr/openIndexPage.do')
-# driver.find_element_by_id("inputSearchAddr").send_keys('test')
-# driver.find_element_by_xpath('''//button[@class="btn_search searchBtn"]''').click()
-# time.sleep(0.5)
-
-# for given_addr in givenaddr_list:
-#     driver.find_element_by_id("keyword").clear()
-#     time.sleep(0.2)
-#     driver.find_element_by_id("keyword").send_keys(given_addr)
-#     time.sleep(0.2)
-#     driver.find_element_by_id("searchButton").click()
-#     time.sleep(0.2)
-#     a = driver.find_element_by_xpath('''//div[@id="list1"]/div[@class="subejct_2"]/span[@class="roadNameText"]''').text
-#     print(a)
-    
-
-
-
-
-
-# driver.get('https://map.naver.com/')
-# driver.implicitly_wait(3)
-
-# for given_addr in givenaddr_list:
-#     driver.find_element_by_xpath('''//div[@class="input_box"]/input''').clear()
-#     time.sleep(0.2)
-#     driver.find_element_by_xpath('''//div[@class="input_box"]/input''').send_keys(given_addr)
-#     time.sleep(0.2)
-#     driver.find_element_by_xpath('''//div[@class="input_box"]/input''').send_keys(Keys.ENTER)
-#     time.sleep(0.2)
-#     try:
-#         a = driver.find_element_by_xpath('''//div[@class="summary_area"]/div''').text
-#         print(a)
-#     except:
-#         print("암것도 업써영")
-
-
-
-
-# driver.get('https://map.kakao.com/')
-# driver.implicitly_wait(3)
-
-# for given_addr in givenaddr_list:
-#     try:
-#         element = WebDriverWait(driver, 3).until(
-#             EC.presence_of_element_located((By.XPATH, '''//div[@class="box_searchbar"]'''))
-#             )
-#     finally:
-#         driver.find_element_by_id("search.keyword.query").clear()
-#         time.sleep(0.2)
-#         driver.find_element_by_xpath('''//div[@class="box_searchbar"]/input''').send_keys(given_addr)
-#         time.sleep(0.2)
-#         driver.find_element_by_xpath('''//div[@class="box_searchbar"]/input''').send_keys(Keys.ENTER)
-#         time.sleep(0.2)
-#         try:
-#             a = driver.find_element_by_xpath('''//div[@class="txt_addr clickArea"]/a''').text
-#             print(a)
-#         except:
-#             print("암것도 업써영")
-
-
-   
-
-
-driver.get('http://kras.seoul.go.kr/land_info/info/baseInfo/baseInfo.do')
-for given_addr in givenaddr_list:
-    r1 = fn.StringHandler.addressToDict(given_addr)
-    fn.Action.isplSearch(driver, r1)
-    r2 = fn.Action.isplPopupCheck(driver, r1)
-    print(r2)
-
-    for key, value in r2.items():
-        dataframe_result[key].append(value)
-    # naverSearch(data_list)
+print("processing...")
+dataframe_result = {
+    '검수내역' : [],
+    '용적률' : [],
+    '참조자료' : [],
+    '특이사항' : []
+}
+for i, v in tqdm(enumerate(tl)):
+    # 1. if given address has multiple items
+    if v in apprd:
+        result_list = compareList(apprd[v])
+        if result_list == 0:
+            dataframe_result['검수내역'].append("사용자 검수 필요")
+            dataframe_result['용적률'].append("")
+            dataframe_result['참조자료'].append("")
+            dataframe_result['특이사항'].append("주소는 확인되나 정보 확인 불가")
+        elif len(apprd[v]) == 1:
+            dataframe_result['검수내역'].append("용적률정비")
+            dataframe_result['용적률'].append(apprd[v][0][1])
+            dataframe_result['참조자료'].append(apprd[v][0][2][:4] + "년 사용(임시)승인허가내역서")
+            dataframe_result['특이사항'].append("")
+        else:
+            dataframe_result['검수내역'].append("용적률정비")
+            dataframe_result['용적률'].append(result_list[0][1])
+            dataframe_result['참조자료'].append(result_list[0][2][:4] + "년 사용(임시)승인허가내역서")
+            dataframe_result['특이사항'].append("중복자료 존재, " + str(apprd[v]))
+    else:
+        dataframe_result['검수내역'].append("확인불가")
+        dataframe_result['용적률'].append("")
+        dataframe_result['참조자료'].append("사용(임시)승인허가내역서")
+        dataframe_result['특이사항'].append("")
 
 raw_data = pd.DataFrame(dataframe_result)
 xlxs_dir=r'C:\Users\user\Desktop\result.xlsx'
