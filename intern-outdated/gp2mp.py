38fbed915c04b0e89e66be1f7221cb6f5f3d4942
electronyoon <electronyoon@gmail.com>
2020-12-09 17:16:28 +0900

    gp2mp가 추가되었읍니다,,

---
+++ intern-outdated/gp2mp.py
@@ -0,0 +1,117 @@
+import os
+import exif
+import datetime
+
+googlePhotoPath = r"C:\Users\user\Pictures\Saved Pictures\\"
+macPhotoPath = r"C:\Users\user\Pictures\iCloud Photos\Photos\\"
+
+
+def detectDuplicate(s):
+    if '1).' in s:
+        return True
+    if '2).' in s:
+        return True
+    if '3).' in s:
+        return True
+    if '_1.' in s:
+        return True
+    if '_2.' in s:
+        return True
+    if '_3.' in s:
+        return True
+    if '-edited.' in s:
+        return True
+    
+    return False
+
+def compareDuplicate(gp_list, mp_list):
+    def gps2coord(t):
+        d, m, s = t
+        coord = d + m/60 + s/3600
+        return coord
+
+    def compareName(s1, s2):
+
+    def compareGps(s1, s2):
+        if s1 == 0 and s2 == 0:
+            pass
+        if s1 == 0 and s2 != 0:
+            pass
+        if s1 != 0 and s2 == 0:
+            pass
+        if s1 != 0 and s2 != 0:
+            pass
+
+    def compareDate(s1, s2):
+        if s1 == 0 and s2 == 0:
+            pass
+        if s1 == 0 and s2 != 0:
+            pass
+        if s1 != 0 and s2 == 0:
+            pass
+        if s1 != 0 and s2 != 0:
+            pass
+
+        date1 = datetime.datetime.strptime(s1,'%Y:%m:%d %H:%M:%S')
+        date2 = datetime.datetime.strptime(s2,'%Y:%m:%d %H:%M:%S')
+
+        if date1 > date2:
+            return s1
+        else: 
+            return s2
+
+    gp_imagedb = exif.Image(googlePhotoPath + gp_list[0])
+    mp_imagedb = exif.Image(macPhotoPath + mp_list[0])
+
+    # export google photo's exif original date
+    try: 
+        gp_list.append(gp_imagedb.datetime_original)
+    except:
+        gp_list.append(0)
+
+    # export mac photo's exif original date
+    try: 
+        mp_list.append(mp_imagedb.datetime_original)
+    except:
+        mp_list.append(0)
+
+    # export google photo's exif gps
+    try: 
+        gp_list.append(gps2coord(gp_imagedb.gps_latitude))
+        gp_list.append(gps2coord(gp_imagedb.gps_longitude))
+    except:
+        gp_list.append(0)
+        gp_list.append(0)
+    
+    # export mac photo's exif gps
+    try: 
+        mp_list.append(gps2coord(mp_imagedb.gps_latitude))
+        mp_list.append(gps2coord(mp_imagedb.gps_longitude))
+    except:
+        mp_list.append(0)
+        mp_list.append(0)
+    
+    new_list = []
+        
+
+
+
+# def main():
+#     gpFileList = []
+#     for f in os.listdir(googlePhotoPath):
+#         t = []
+#         t.append(f)
+#         gpFileList.append(t)
+
+#     mpFileList = []
+#     for f in os.listdir(macPhotoPath):
+#         t = []
+#         t.append(f)
+#         mpFileList.append(t)
+
+#     print(gpFileList)
+
+
+
+# if __name__ == "__main__":
+#     main()
--- intern-outdated/main.py
+++ intern-outdated/main.py
@@ -17,9 +17,9 @@ def removeBlanks(s):
 def compareList(l):
     # remove list with abnormal ratio
     for i in l:
-        if not isinstance(i[1], str):
-            l.remove(i)
-        if i[1] <= 0:
+        # if not isinstance(i[1], str):
+        #     l.remove(i)
+        if float(i[1]) <= 0.0:
             l.remove(i)
     
     # compare dates
@@ -32,7 +32,7 @@ def compareList(l):
     except:
         return 0
 
-    # find news
+    # find youngest(s) -- could be several
     result_list = []
     for i in l:
         if i[2] == youngest:
@@ -43,12 +43,12 @@ def compareList(l):
 # addr_column = "A"
 # addr_to_num = sum([v*26**(len(addr_column)-i-1) for i, v in enumerate([ord(s)-64 for s in addr_column])])-1
 print("reading approved excel file as pandas...")
-data = pd.read_excel(r'C:\Users\user\Desktop\사용(임시)승인허가현황조회 전체_강서구_정렬_1120.xls')
-df = data.loc[:, ['건축구분', '대지위치', '용적률(%)', '사용승인일']]
+file1 = '사용(임시)승인허가현황조회 전체_강서구_정렬_1120.xls'
+data = pd.read_excel(r'C:\Users\user\Desktop' + '\\' + file1)
+df = data.loc[:, ['건축구분', '대지위치', '건폐율(%)', '사용승인일']]
 l = df.values.tolist()
 for i in l:
     i[1] = removeBlanks(i[1])
-
 apprd = {}
 for i in l:
     address = i[1]
@@ -61,8 +61,9 @@ for i in l:
         apprd[address].append(i)
 
 print("reading target excel file as pandas...")
-data2 = pd.read_excel(r'C:\Users\user\Desktop\뉴딜데이터정비_건축물대장_강서구_표시현황정비(대지면적외항목)_201126_검수1_김영철김소현.xlsx',
-                    sheet_name='용적률')
+file2 = '뉴딜데이터정비_건축물대장_강서구_표시현황정비(대지면적외항목)_201201_검수2.xlsx'
+data2 = pd.read_excel(r'C:\Users\user\Desktop' + "\\" + file2,
+                    sheet_name='건폐율')
 df2 = data2.iloc[:, 2:4]
 l2 = df2.values.tolist()
 del l2[0]
@@ -73,32 +74,33 @@ for i in l2:
 print("processing...")
 dataframe_result = {
     '검수내역' : [],
-    '용적률' : [],
+    '건폐율' : [],
     '참조자료' : [],
-    '특이사항' : []
+    '특이사항' : [],
+    '주소' : []
 }
-for i, v in tqdm(enumerate(tl)):
-    # 1. if given address has multiple items
+for i, v in enumerate(tl):
+    dataframe_result['주소'].append(v)
     if v in apprd:
         result_list = compareList(apprd[v])
         if result_list == 0:
             dataframe_result['검수내역'].append("사용자 검수 필요")
-            dataframe_result['용적률'].append("")
+            dataframe_result['건폐율'].append("")
             dataframe_result['참조자료'].append("")
-            dataframe_result['특이사항'].append("주소는 확인되나 정보 확인 불가")
+            dataframe_result['특이사항'].append("승인허가현황에 등록된 주소이나 기계판독 할 수 없음")
         elif len(apprd[v]) == 1:
-            dataframe_result['검수내역'].append("용적률정비")
-            dataframe_result['용적률'].append(apprd[v][0][1])
+            dataframe_result['검수내역'].append("건폐율정비")
+            dataframe_result['건폐율'].append(apprd[v][0][1])
             dataframe_result['참조자료'].append(apprd[v][0][2][:4] + "년 사용(임시)승인허가내역서")
             dataframe_result['특이사항'].append("")
         else:
-            dataframe_result['검수내역'].append("용적률정비")
-            dataframe_result['용적률'].append(result_list[0][1])
+            dataframe_result['검수내역'].append("건폐율정비")
+            dataframe_result['건폐율'].append(result_list[0][1])
             dataframe_result['참조자료'].append(result_list[0][2][:4] + "년 사용(임시)승인허가내역서")
             dataframe_result['특이사항'].append("중복자료 존재, " + str(apprd[v]))
     else:
         dataframe_result['검수내역'].append("확인불가")
-        dataframe_result['용적률'].append("")
+        dataframe_result['건폐율'].append("")
         dataframe_result['참조자료'].append("사용(임시)승인허가내역서")
         dataframe_result['특이사항'].append("")
 
